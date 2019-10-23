5e67fe25b7db3bde8e51316fd6c37a8305d37cbb
Joshua Franklin <joshuafranklin26@gmail.com>
2019-10-23 14:03:53 -0600

    Work on lab4

--- lab4.py
+++ lab4.py
@@ -20,8 +20,8 @@ SCALE = 3
 IR_THRESHOLD = 500
 CYCLE_TIME = 0.05
 
-WORLD_SIZE_X = 60
-WORLD_SIZE_Y = 42
+MAP_SIZE_X = 60
+MAP_SIZE_Y = 42
 
 PING_COOLDOWN = 1
 
@@ -106,7 +106,6 @@ class Sparki(object):
     def servo_angle(self, angle):
         assert -80 < angle < 80
 
-        self._servo_angle = angle
         self.pub_servo.publish(angle)
 
     def _update_odometry(self, pose):
@@ -121,65 +120,74 @@ class Sparki(object):
 
 
 class Map(object):
-    def __init__(self):
+    def __init__(self, sparki):
         pygame.init()
         pygame.display.set_caption("Sparki's Map")
 
-        self.world = [[False] * WORLD_SIZE_X for _ in range(WORLD_SIZE_Y)]
+        self.sparki = sparki
+        self.map = [[False] * MAP_SIZE_X for _ in range(MAP_SIZE_Y)]
 
         self.sparki_img = pygame.transform.scale(pygame.image.load('sparki.png'), (SPARKI_IMG_WIDTH, SPARKI_IMG_HEIGHT))
         self.map_img = pygame.transform.scale(pygame.image.load('map.png'), (SCREEN_WIDTH, SCREEN_HEIGHT))
 
+        # Draw the grid on the map.
+        for row in range(MAP_SIZE_Y):
+            pygame.draw.line(self.map_img, (100, 100, 100), (row * 10, 0), (row * 10, SCREEN_HEIGHT))
+        for col in range(MAP_SIZE_X):
+            pygame.draw.line(self.map_img, (100, 100, 100), (0, col * 10), (SCREEN_WIDTH, col * 10))
+
         self.screen = pygame.display.set_mode([SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE])
         self.fake_screen = pygame.surface.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    def draw(self, pose):
+    def draw(self):
         self.fake_screen.blit(self.map_img, (0, 0))
 
         self.fake_screen.blit(
-            pygame.transform.rotate(self.sparki_img, np.degrees(pose.theta)),
+            pygame.transform.rotate(self.sparki_img, np.degrees(self.sparki.pose.theta)),
             (
-                (-pose.y * 1000) + ZERO_OFFSET_X - (SPARKI_IMG_WIDTH // 2),
-                (-pose.x * 1000) + ZERO_OFFSET_Y - (SPARKI_IMG_HEIGHT // 2),
+                (-self.sparki.pose.y * 1000) + ZERO_OFFSET_X - (SPARKI_IMG_WIDTH // 2),
+                (-self.sparki.pose.x * 1000) + ZERO_OFFSET_Y - (SPARKI_IMG_HEIGHT // 2),
             )
         )
 
-        for row in range(WORLD_SIZE_Y):
-            for col in range(WORLD_SIZE_X):
-                if self.world[row][col]:
-                    pygame.draw.rect(self.fake_screen, (255, 0, 0), (row * 10, col * 10, 10, 10))
+        for row in range(MAP_SIZE_Y):
+            for col in range(MAP_SIZE_X):
+                if self.map[row][col]:
+                    pygame.draw.rect(self.fake_screen, (255, 0, 0), (row * 10 + ZERO_OFFSET_X, col * 10 + ZERO_OFFSET_Y, 10, 10))
 
         self.screen.blit(pygame.transform.scale(self.fake_screen, (SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE)), (0, 0))
         pygame.display.update()
 
-    def add_point(self, pose, servo_angle, dist):
-        print('X:', pose.x)
-        print('Y:', pose.y)
-        print('T:', pose.theta)
-        print('A:', servo_angle)
-        print('D:', dist)
+    def add_point(self):
+        self.sparki.us_distance = 10
+        print('X:', self.sparki.pose.x)
+        print('Y:', self.sparki.pose.y)
+        print('T:', self.sparki.pose.theta)
+        print('A:', self.sparki.servo_angle)
+        print('D:', self.sparki.us_distance)
 
         rotation_matrix = np.empty(shape=(3, 3))
-        rotation_matrix[0][0] = np.cos(pose.theta)
-        rotation_matrix[0][1] = -(np.sin(pose.theta))
-        rotation_matrix[0][2] = pose.x
-        rotation_matrix[1][0] = np.sin(pose.theta)
-        rotation_matrix[1][1] = np.cos(pose.theta)
-        rotation_matrix[1][2] = pose.y
+        rotation_matrix[0][0] = np.cos(self.sparki.pose.theta)
+        rotation_matrix[0][1] = -(np.sin(self.sparki.pose.theta))
+        rotation_matrix[0][2] = self.sparki.pose.x * 100
+        rotation_matrix[1][0] = np.sin(self.sparki.pose.theta)
+        rotation_matrix[1][1] = np.cos(self.sparki.pose.theta)
+        rotation_matrix[1][2] = self.sparki.pose.y * 100
         rotation_matrix[2][0] = 0
         rotation_matrix[2][1] = 0
         rotation_matrix[2][2] = 1
 
-        obj_x = dist * np.cos(servo_angle)
-        obj_y = dist * np.sin(servo_angle)
+        obj_x = self.sparki.us_distance * np.cos(self.sparki.servo_angle)
+        obj_y = self.sparki.us_distance * np.sin(self.sparki.servo_angle)
         robot_coords = np.empty(shape=(3, 1))
         robot_coords[0][0] = obj_x
         robot_coords[1][0] = obj_y
         robot_coords[2][0] = 1
 
         homo_matrix = np.matmul(rotation_matrix, robot_coords)
-        # print('Added:', homo_matrix)
-        self.world[int(homo_matrix[0, 0])][int(homo_matrix[1, 0])] = True
+        print('Added:', homo_matrix)
+        print('Plotting:', int(homo_matrix[1, 0]), int(homo_matrix[0, 0]))
+        self.map[-int(homo_matrix[1, 0])][int(homo_matrix[0, 0])] = True
 
 
 class Lab4(object):
@@ -187,11 +195,11 @@ class Lab4(object):
         rospy.init_node('Lab4')
 
         self.sparki = Sparki()
-        self.map = Map()
+        self.map = Map(self.sparki)
 
         self.sparki.reset_odometry()
         self.sparki.motor_speeds = 0, 0
-        self.sparki.servo_angle = -40
+        self.sparki.servo_angle = 0
 
     def _loop(self):
         self.sparki.request_ultrasonic()
@@ -206,9 +214,10 @@ class Lab4(object):
         else:
             self.sparki.motor_speeds = 1, 1
 
-        if 0 < self.sparki.us_distance < 30:
-            self.map.add_point(self.sparki.pose, self.sparki.servo_angle, self.sparki.us_distance)
-        self.map.draw(self.sparki.pose)
+        # if 0 < self.sparki.us_distance < 30:
+            self.map.add_point()
+
+        self.map.draw()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
